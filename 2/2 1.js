d84b7cbf923b30c10d18454bdbc66f14ab70346c
Rio-Meow <rrrio.meow@gmail.com>
2025-03-17 00:16:34 +0700

    добавлен 2 и 3 блок  задач по js

---
+++ 2/2 1.js
@@ -0,0 +1,9 @@
+let password = '123'; 
+
+let userInput = prompt('Введите пароль');
+
+if (userInput === password) {
+  alert('Пароль введен верно');
+} else {
+  alert('Пароль введен неправильно');
+}
