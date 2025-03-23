97256fae9af88821132528c4ac406f7cc1f509be
Rio-Meow <rrrio.meow@gmail.com>
2025-03-24 00:16:58 +0700

    Revert "добавлен 2 и 3 блок  задач по js"

--- 2/2 1.js
+++
@@ -1,9 +0,0 @@
-let password = '123'; 
-
-let userInput = prompt('Введите пароль');
-
-if (userInput === password) {
-  alert('Пароль введен верно');
-} else {
-  alert('Пароль введен неправильно');
-}
