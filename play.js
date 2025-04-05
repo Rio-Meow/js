836bd445d223d585512d30db99f784bb6b2f6336
Rio-Meow <rrrio.meow@gmail.com>
2025-04-05 16:16:51 +0700

    кнопка и выравнивание

--- play.js
+++ play.js
@@ -1,38 +1,38 @@
+const playButton = document.getElementById("play1");
+
 function guessTheNumber() {
     const randomNumber = Math.floor(Math.random() * 100) + 1;
-  
     let guess;
 
     function askForGuess() {
-      guess = prompt("Угадайте число от 1 до 100:");
+        guess = prompt("Угадайте число от 1 до 100:");
 
-      if (guess === null) {
-        alert("Игра отменена.");
-        return;
-      }
-  
-      guess = parseInt(guess);
-  
-      if (isNaN(guess)) {
-        alert("Пожалуйста, введите число.");
-        askForGuess(); 
-        return;
-      }
-  
-      if (guess === randomNumber) {
-        alert(`Поздравляю! Вы угадали число ${randomNumber}!`);
-      } else if (guess < randomNumber) {
-        alert("Загаданное число больше.");
-        askForGuess(); 
-      } else {
-        alert("Загаданное число меньше.");
-        askForGuess(); 
-      }
+        if (guess === null) {
+            alert("Игра отменена.");
+            return;
+        }
+
+        guess = parseInt(guess);
+
+        if (isNaN(guess)) {
+            alert("Пожалуйста, введите число.");
+            askForGuess();
+            return;
+        }
+
+        if (guess === randomNumber) {
+            alert(`Поздравляю! Вы угадали число ${randomNumber}!`);
+        } else if (guess < randomNumber) {
+            alert("Загаданное число больше.");
+            askForGuess();
+        } else {
+            alert("Загаданное число меньше.");
+            askForGuess();
+        }
     }
-  
-    
+
     alert("Я загадал число от 1 до 100. Попробуйте угадать!");
     askForGuess();
-  }
-  
-  guessTheNumber();
+}
+
+playButton.addEventListener("click", guessTheNumber);
